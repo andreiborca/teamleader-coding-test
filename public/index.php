590a821a6f49f3785cc450a13ef4922b5507f5ed
Andrei Borca <andreiborca@gmail.com>
2024-10-06 20:51:30 +0300

    feat: Registered the DiscountsController under route '/discounts/caclulate/'

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
+use App\Controllers\DiscountsController;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
@@ -11,5 +12,7 @@ $app->get('/', function (Request $request, Response $response, $args) {
 	$response->getBody()->write("Hello, Slim Framework on Docker!");
 	return $response;
 });
+
+$app->post("/discounts/calculate", [DiscountsController::class, "calculate"]);
 
 $app->run();
