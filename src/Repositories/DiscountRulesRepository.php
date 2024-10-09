aeb9c657b9fa20e139498cd51092705440d3fd1a
Andrei Borca <andreiborca@gmail.com>
2024-10-09 19:40:51 +0300

    feat: Add repository class for DiscountRule entity

---
+++ src/Repositories/DiscountRulesRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Entities\DiscountRule;
+
+class DiscountRulesRepository
+{
+	/**
+	 * @var DiscountRule[]
+	 */
+	private array $discountRules = [];
+
+	public function __construct()
+	{
+		$discountRules = json_decode(
+			file_get_contents(realpath(".") . "/../data/discount-rule0s.json"),
+			true
+		);
+
+		foreach ($discountRules as $discountRule) {
+			$this->discountRules[] = new DiscountRule(
+				$discountRule["id"],
+				$discountRule["type"],
+				$discountRule["discount-rules"],
+			);
+		}
+	}
+
+	public function getAll() : array {
+		return $this->discountRules;
+	}
+}
